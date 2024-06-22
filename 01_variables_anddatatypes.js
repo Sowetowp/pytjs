ffe0d489d202308b87fc976e4b94b10221091fe6
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-22 19:28:31 +0100

    F

---
+++ 01_variables_anddatatypes.js
@@ -0,0 +1,41 @@
+// malik waz ere
+/*let malik=             7;
+var wale = 8;
+const kizzo = 0;*/
+
+let henry
+henry = 1e+74
+
+console.log(henry)
+// alert(henry)
+// henry = "fair"
+document.write(henry)
+document.getElementById("root").innerHTML = henry
+
+// types of data
+// numeric
+// bigintiger
+// strings
+// boolean 
+// objects
+// null
+// undefined
+document.write(typeof henry)
+
+henry = "8 898"
+document.write(typeof henry)
+document.write(henry)
+henry = true
+document.write(henry)
+
+henry = {
+    name: {}
+}
+document.write(typeof henry)
+henry = [6, 8, 9, "kkk"]
+document.write( henry)
+// console.log(henry)
+henry = null
+document.write( henry)
+let kizzo
+document.write(typeof kizzo)
