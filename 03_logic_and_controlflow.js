81ec04da853e32b8985a20e4a438295da15518f7
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-24 14:25:05 +0100

    auto-commit

--- 03_logic_and_controlflow.js
+++ 03_logic_and_controlflow.js
@@ -25,5 +25,5 @@ if(chioma <= 4){
 }else if(chioma < 18){
     document.write("chioma is an teenager")
 }else if(chioma >= 18 && chioma <= 19){
-    document.write("chioma is an adult")
+    document.write("chioma is an adult that is also a teenager")
 }
