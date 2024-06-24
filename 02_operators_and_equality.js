8f0c6a3788fe6762842a482074722fb614dd9ee8
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-24 12:22:38 +0100

    auto-commit

--- 02_operators_and_equality.js
+++ 02_operators_and_equality.js
@@ -1,12 +1,12 @@
 let malik = 88
 var ayodeji = 99
-d(malik + ayodeji)
-d(malik - ayodeji)
-d(malik * ayodeji)
-d(malik / ayodeji)
-d(malik ** ayodeji)
-d(malik % ayodeji)
+document.write(malik + ayodeji)
+document.write(malik - ayodeji)
+document.write(malik * ayodeji)
+document.write(malik / ayodeji)
+document.write(malik ** ayodeji)
+document.write(malik % ayodeji)
 malik += ayodeji
-d(malik)
+document.write(malik)
 
 
