26a2d84b66c13a7fcd9e9a186e74ddb79bbd6823
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 12:41:38 +0100

    auto-commit

--- 03_logic_and_controlflow.js
+++ 03_logic_and_controlflow.js
@@ -65,4 +65,4 @@ switch (superHero) {
 }
 
 superHero === "Hulk" ? console.log("I am Hulk") : console.log("I am not Hulk")
-superHero === "Hulk" ? console.log("I am Hulk") : superHero === "thor" ? console.log("I have my hammer") : console.log("I anm ")
+superHero === "Hulk" ? console.log("I am Hulk") : superHero === "thor" ? console.log("I have my hammer") : console.log("I am neither hulk nor thor")
