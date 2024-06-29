6d66e02b9c8fc48d840f89ca6f3184dd3905e02a
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 13:06:55 +0100

    auto-commit

--- 04_loops.js
+++ 04_loops.js
@@ -5,5 +5,5 @@
 // for of loop
 
 for (let i = 1; i < 10; i++){
-    i % 2 === 0 ? console.log("I am an even number") : ""
+    i % 2 === 0 ? console.log("I am an even number") : "I am an odd number"
 }
