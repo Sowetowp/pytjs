a74c5dff4486cf3a4a3270be45e676d35a22f072
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 12:40:11 +0100

    auto-commit

--- 03_logic_and_controlflow.js
+++ 03_logic_and_controlflow.js
@@ -40,7 +40,7 @@ if (sum % 2 === 0) {
     console.log("i am an odd number")
 }
 
-const superHero = "Hulk"
+const superHero = "thor"
 switch (superHero) {
     case "thor":
         console.log("I have my hammer")
