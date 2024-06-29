abb60b3ba5a7ed62c25b1d32b45afb055c50775d
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-29 13:34:47 +0100

    auto-commit

--- 04_loops.js
+++ 04_loops.js
@@ -17,3 +17,9 @@ do {
     console.log(a)
     a++
 } while (a <= 10);
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
