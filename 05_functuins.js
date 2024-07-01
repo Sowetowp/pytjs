1479e634ba15b7c5d1e1bc9ca1b62f8c4874b2c4
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-01 13:11:03 +0100

    auto-commit

--- 05_functuins.js
+++ 05_functuins.js
@@ -1,4 +1,5 @@
-function myFunction(chioma1) {
-    console.log(chioma1)
+function myFunction(chioma1, chioma2, chioma3) {
+    console.log(chioma2)
+    console.log(chioma1 * chioma2)
 }
-myFunction(1, 2)
+myFunction(1)
