e69f6903e69bfa485f11f232100d9b41587c7801
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-01 14:01:03 +0100

    auto-commit

--- 05_functuins.js
+++ 05_functuins.js
@@ -4,25 +4,25 @@ function myFunction(chioma1, chioma2, chioma3) {
 }
 myFunction(1, 2, 3)
 
-function areaOfACircle (r){
-    console.log(Math.random() * 3.142 * r**2)
+function areaOfACircle(r) {
+    console.log(Math.random() * 3.142 * r ** 2)
 }
 areaOfACircle(2)
 
 const myArray = [
-    {start: 1, end: 3},
-    {start: 2, end: 4},
-    {start: 3, end: 5},
-    {start: 0, end: 6},
-    {start: 5, end: 7},
-    {start: 8, end: 9},
-    {start: 5, end: 9}
+    { start: 1, end: 3 },
+    { start: 2, end: 4 },
+    { start: 3, end: 5 },
+    { start: 0, end: 6 },
+    { start: 5, end: 7 },
+    { start: 8, end: 9 },
+    { start: 5, end: 9 }
 ]
-const timeScheduling = ()=>{
+const timeScheduling = () => {
     const output = []
-    for (let i = 0; i<= myArray.length; i++) {
-        if(){
-    
+    for (let i = 0; i <= myArray.length; i++) {
+        if () {
+
         }
     }
 }
