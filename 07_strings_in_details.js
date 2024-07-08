9f8b718669d2c45074479f3c7e6bf9c54b56b1fc
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-08 13:37:40 +0100

    auto-commit

--- 07_strings_in_details.js
+++ 07_strings_in_details.js
@@ -6,4 +6,4 @@ console.log(malik.toLowerCase())
 console.log(chioma.toString())
 console.log(malik.slice(3))
 
-console.log(malik.slice(0, 1).toUpperCase() + malik.slice(1))
+console.log(malik.[0].toUpperCase() + malik.slice(1))
