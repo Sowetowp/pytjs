84f6a4a8147813b2731aa6c19fb4a313ed69d280
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 11:57:29 +0100

    auto-commit

--- 08_array_in_details.js
+++ 08_array_in_details.js
@@ -2,4 +2,5 @@ const myArray = ["ope", "eri", "wunmi", "chioma"]
 console.log(myArray.join(" . "))
 console.log(myArray.length)
 console.log(myArray.reverse())
-console.log(myArray.push("afa"))
+myArray.push("afa")
+console.log()
