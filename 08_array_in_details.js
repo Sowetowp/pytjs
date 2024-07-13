0a24827d6fe2f994d43a12a4c55c020c27bd68e2
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 12:44:17 +0100

    auto-commit

--- 08_array_in_details.js
+++ 08_array_in_details.js
@@ -21,6 +21,7 @@ console.log(myArray.at(-1))
 const myarr2 = [[1, 2, 3], ["malik", "david"]]
 console.log(myarr2.flat())
 const ph = []
+
 const mm = myArray.forEach((val, index, arr)=>{
     ph.push(val + "olodo")
 })
