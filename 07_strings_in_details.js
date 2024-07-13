6ee27382aba7858d6d51151dc0a48f618d1ffa94
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 11:40:15 +0100

    auto-commit

--- 07_strings_in_details.js
+++ 07_strings_in_details.js
@@ -9,3 +9,6 @@ console.log(malik.slice(3))
 console.log(malik[0].toUpperCase() + malik.slice(1))
 console.log(malik.split("|"))
 console.log(malik.trim())
+ const a = "     malik     "
+ const b = "malik"
+ console.log(a === b)
