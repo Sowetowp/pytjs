348e77bcfc10e097c79596d3f36c95d284189628
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 12:30:57 +0100

    auto-commit

--- 08_array_in_details.js
+++ 08_array_in_details.js
@@ -19,6 +19,7 @@ console.log(myArray.toString())
 console.log(myArray[myArray.length - 1])
 console.log(myArray.at(-1))
 const myarr2 = [[1, 2, 3], ["malik", "david"]]
+
 myArray.forEach((val, index, arr)=>{
     console.log(val, index)
 })
