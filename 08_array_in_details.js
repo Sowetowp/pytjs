2fb13854f92375c06171fb3d6d28e905cfc82f25
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 12:25:26 +0100

    auto-commit

--- 08_array_in_details.js
+++ 08_array_in_details.js
@@ -16,7 +16,7 @@ console.log(myArray.slice(1, 3))
 console.log(myArray)
 myArray.splice(1, 2, "elvis", "david")
 console.log(myArray.toString())
-console.log(myArray[0])
+console.log(myArray[])
 myArray.forEach((val, index, arr)=>{
     console.log(val, index)
 })
