19fa4c1ab97a15293ac8cc57757b5931dae897c3
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 12:49:54 +0100

    auto-commit

--- 08_array_in_details.js
+++ 08_array_in_details.js
@@ -28,6 +28,6 @@ const mm = myArray.forEach((val, index, arr)=>{
 console.log(ph)
 
 const mapped = myArray.map((val, index, arr)=>{
-    console.log(val, index, arr)
+    val + " " + "omo ologo"
 })
 console.log(mapped)
