51fb8461279a6932c408f97b1b05acde4d0d783d
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-13 13:15:35 +0100

    auto-commit

--- 08_array_in_details.js
+++ 08_array_in_details.js
@@ -38,6 +38,6 @@ const filt = myArray.filter((val)=>{
 console.log(filt)
 let kk = [1, 2, 3]
 const reduced = kk.reduce((acc, val, ind, arr)=>{
-    acc[val] = val
+   return acc[val] = val
 }, {malik: "malik"})
 console.log(reduced)
